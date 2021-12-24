b921a051ac1c9f8dc76dbe578936824ae617bad1
Khimsreyhon <sreyhon.khim@student.passerellesnumeriques.org>
2021-12-24 10:25:59 +0700

    #20 create ero user

--- front/src/main.js
+++ front/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import base_form from '@/components/ui/base-form.vue'
+import base_dailog from '@/components/ui/dailog.vue'
 Vue.config.productionTip = false
 Vue.component('Base-form', base_form)
+Vue.component('Base-dailog', base_dailog)
 
 
 new Vue({
