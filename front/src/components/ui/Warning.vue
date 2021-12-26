d860c2d500b1e9d693c0f04872cef92b1d4338a8
chum-ch <yoeurnchum@gmail.com>
2021-12-26 14:40:10 +0700

    #10 dailog warning

--- front/src/components/ui/Warning.vue
+++ front/src/components/ui/Warning.vue
@@ -1,57 +1,56 @@
 <template>
   <div class="all-card">
-      <v-card>
-        <div class="image">
-            <img
-              src="https://www.passerellesnumeriques.org/wp-content/uploads/2016/03/pn-logo.png"
-              alt=""
-              width="110"
-            />
-        </div>
-      </v-card>
-        <v-alert class="alert"
-        :value="true"
-        type="error"
-      >
-        <span>Your sign up account failed! Please contact to ERO Team.</span>
-      </v-alert>
+    <v-card width="50%" class="ma-auto">
+      <div class="image pa-2 pl-5">
+        <img
+          src="https://www.passerellesnumeriques.org/wp-content/uploads/2016/03/pn-logo.png"
+          alt=""
+          width="110"
+        />
+      </div>
+      <div class="d-flext mt-3" align="center" justify="space-around">
+        <span>
+          <v-icon large class="text-h2 red--text">mdi-alert</v-icon>
+        </span>
+        <h2 class="red--text">
+          Failed create account, you are not PNC alumni.
+        </h2>
+      </div>
 
-        <v-container fluid class="d-flex" v-if="show_details">
-          <v-card-text class="text-h5">
-            <h4>Please contact to ERO Team or Admin!</h4>
-            <h5>Admin</h5>
-            <div class="txt">
-              <span class="sub-txt">Name</span>
-              <span>: Chum</span>
-            </div>
-            <div class="txt">
-              <span class="sub-txt">Phone</span>
-              <span>: +855 54 12 123</span>
-            </div>
-            <div class="txt">
-              <span class="sub-txt">Email</span>
-              <span>: chum.yoeurn@gmail.com</span>
-            </div>
-            <h5>ERO Team</h5>
-            <div class="txt">
-              <span class="sub-txt">Name</span>
-              <span>: Prey Veng</span>
-            </div>
-            <div class="txt">
-              <span class="sub-txt">Phone</span>
-              <span>: +855 54 12 123</span>
-            </div>
-            <div class="txt">
-              <span class="sub-txt">Email</span>
-              <span>: chum.yoeurn@gmail.com</span>
-            </div>
-          </v-card-text>
-          <router-link to="/warning" class="btn blue"><v-icon class="icon blue"
-            left
-          >
-            mdi-arrow-left
-          </v-icon>Back</router-link> 
-        </v-container>
+      <v-container fluid v-if="show_details">
+        <v-card-text class="pb-10">
+          <h3>Please contact to ERO Team or Admin!</h3>
+          <h4 class="cyan--text">Admin</h4>
+          <div class="txt">
+            <h5 class="sub-txt">Name</h5>
+            <span>: XXXXXX</span>
+          </div>
+          <div class="txt">
+            <h5 class="sub-txt">Phone</h5>
+            <span>: +855 54 12 123</span>
+          </div>
+          <div class="txt">
+            <h5 class="sub-txt">Email</h5>
+            <span>: XXXXX.XXXXXX@admin.passerellesnumeriques.org</span>
+          </div>
+          <h4 class="cyan--text">ERO Team</h4>
+          <div class="txt">
+            <h5 class="sub-txt">Name</h5>
+            <span>: XXXXXX</span>
+          </div>
+          <div class="txt">
+            <h5 class="sub-txt">Phone</h5>
+            <span>: +855 54 12 123</span>
+          </div>
+          <div class="txt">
+            <h5 class="sub-txt">Email</h5>
+            <span>: XXXXX.XXXXXX@ero.passerellesnumeriques.org</span>
+          </div> 
+          <slot></slot>
+         
+        </v-card-text>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
@@ -59,18 +58,16 @@
 export default {
   data() {
     return {
-     show_details: true,
-      cruds: [
-        
-      ],
+      show_details: true,
+      cruds: [],
     };
   },
   methods: {
-    details(){
-      console.log('dd');
-      this.show_details = !this.show_details
+    dd(){console.log('dd');},
+    details() {
+      this.show_details = !this.show_details;
     },
-    image(e){
+    image(e) {
       console.log(e.target.files[0]);
     },
   },
@@ -82,45 +79,14 @@ export default {
 .txt {
   display: flex;
   margin-left: 4%;
-  
 }
 .sub-txt {
-  width: 300px;
+  width: 220px;
 }
 .txt span {
   margin: 5px;
 }
-.image{
-  background: #44C7F5;
-
-}
-img{
-  margin-left: 5%;
-}
-.all-card{
-  width: 60%;
-  margin-left: 20%;
-  background: rgb(247, 248, 246);
-}
-
-.btn {
-  margin-right: 5%;
-  display: flex;
-  color: #ffffff;
-  border: 1px solid #ffffff;
-  box-shadow: inset 0 0 0 0 #ffffff;
-  border-radius: 10px;
-  padding-right: 20px;
-  padding-left: 20px;
-  padding-top: 6px;
-  padding-bottom: 6px;
-  margin-top: 50%;
-}
-.icon{
-  color: white;
-}
-.btn:hover {
-  color: #44c7f5;
-  box-shadow: inset 150px 0 0 0 #ffffff;
+.image {
+  background: #44c7f5;
 }
 </style>
