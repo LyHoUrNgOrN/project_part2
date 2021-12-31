fb7d276d51476213d2bc49788f023f26c2d68759
saravey2021 <myemail@example.com>
2021-12-31 07:48:47 +0700

    #22 create information company

--- front/src/components/views/Alumni_Company.vue
+++ front/src/components/views/Alumni_Company.vue
@@ -180,8 +180,8 @@
                     </v-row>
                   </v-card-text>
                   <v-card-actions class="justify-end">
-                    <v-btn color="cyan white--text" text @click="cancle">Cancel</v-btn>
-                    <v-btn text @click="my_company">Save</v-btn>
+                    <v-btn text @click="cancle">Cancel</v-btn>
+                    <v-btn color="blue white--text" text @click="createInfoCompany">Save</v-btn>
                   </v-card-actions>
                 </v-card>
               </template>
@@ -189,7 +189,7 @@
           </v-col>
         </v-row>
       </v-card>
-      <h1>Work experince</h1>
+  
     </v-main>
   </div>
 </template>
@@ -217,6 +217,7 @@ export default {
       hr_name: "",
       ifHaveInfo : true,
       company_name: "",
+      companyId: "",
       hr_email: "",
       company_phone: "",
       hr_phone: "",
@@ -228,8 +229,15 @@ export default {
       view_company_phone: "",
       id: 0,
       cruds: [],
+      user: [],
 
-      show_details: true
+      show_details: true,
+
+      //
+      newInfo: [],
+
+
+      //
     };
   },
   methods: {
@@ -237,68 +245,94 @@ export default {
       this.dialog = false;
     },
     close() {
-      this.getUser();
+      // this.getUser();
+      // this.getUserInfo();
       this.dialog = false;
     },
-    my_company() {
-      let user_id = JSON.parse(localStorage.getItem("user"));
-      this.id = user_id.id;
-      console.log(this.id);
-      let info = {
-        user_id: this.id,
-        current_position: this.current_position,
-        company_name: this.company_name,
-        company_phone: this.company_phone,
-        company_email: this.company_email,
-        company_address: this.company_address,
-        company_website: this.company_website,
-        hr_name: this.hr_name,
-        hr_email: this.hr_email,
-        hr_phone: this.hr_phone
-      };
-      axios.post("/companies", info).then(res => {
-        console.log(res.data);
-        
-        this.cancle();
-        this.companies = res.data;
+    createInfoCompany() {
+      let newInfo = new FormData();
+      newInfo.append("current_position", this.current_position);
+      newInfo.append("company_name", this.company_name);
+      newInfo.append("company_phone", this.company_phone); 
+      newInfo.append("company_email", this.company_phone); 
+      newInfo.append("company_address", this.company_address);
+      newInfo.append("company_website", this.company_website); 
+      newInfo.append("hr_name", this.hr_name); 
+      newInfo.append("hr_email", this.hr_email); 
+      newInfo.append("hr_phone", this.hr_phone);
+      axios.post("/companies", newInfo)
+        .then((res) => {
+          let createInfo = {
+            user_id: JSON.parse(localStorage.getItem("user")).id,
+            company_id: res.data.data.id,
+            role: "creator",
+          };
+
+        }).catch((error) => {
+          this.closeDialog(true);
+          this.message = error.response.data.message;
+
       });
+      // let user_id = JSON.parse(localStorage.getItem("user"));
+      // let user_detail_id = JSON.parse(localStorage.getItem("userdetail"));
+      // this.id = user_id.id[0];
+      // this.id = user_detail_id.id[0];
+      // let info = {
+
+      //   user_id: this.id[0],
+      //   user_detail_id: this.id[0],
+      //   current_position: this.current_position,
+      //   company_name: this.company_name,
+      //   company_phone: this.company_phone,
+      //   company_email: this.company_email,
+      //   company_address: this.company_address,
+      //   company_website: this.company_website,
+      //   hr_name: this.hr_name,
+      //   hr_email: this.hr_email,
+      //   hr_phone: this.hr_phone
+      // };
+      // axios.post("/companies", info).then(res => {
+      //   console.log(res.data);
+      //   this.dialog = true
+      //   this.cancle();
+      //   this.companies = res.data;
+      // });
+
+    
     },
 
-    getUser() {
-      let user = JSON.parse(localStorage.getItem("user"));
-      axios
-        .get("/companies/" + user.id)
-        .then(result => {
-          if(result.data[0].company_name == undefined){
-            this.ifHaveInfo = true;
-            console.log(result.data)
-          }else{
-            this.ifHaveInfo = false;
-          }
-          this.view_company_name = result.data[0].company_name;
-          this.view_company_email = result.data[0].company_email;
-          this.view_company_phone = result.data[0].company_phone;
-          this.cruds = [
+    getUserInfo(){
+      this.user = JSON.parse(localStorage.getItem('user'));
+      axios.get('/companies/' + this.user.id).then(res=> {
+      this.companyId = res.data[0];
+      this.view_company_name = this.companyId.company_name;
+      this.view_company_email = this.companyId.company_email;
+      this.view_company_phone = this.companyId.company_phone;
+      this.cruds = [
             ["About Company *"],
-            ["Current Position", result.data[0].current_position],
-            ["Company Name", result.data[0].company_name],
-            ["Company Email", result.data[0].company_email],
-            ["Company Phone", result.data[0].company_phone],
-            ["Company Website", result.data[0].company_website],
-            ["Company Address", result.data[0].company_address],
-            ["Human Resources *"],
-            ["HR Name", result.data[0].hr_name],
-            ["HR Email", result.data[0].hr_email],
-            ["HR Phone", result.data[0].hr_phone]
-          ];
-        })
-        .catch(err => {
-          console.log(err.response.data.message);
-      });
+            ["Current Position", this.companyId.current_position],
+            ["Company Name", this.companyId.company_name],
+            ["Company Email", this.companyId.company_email],
+            ["Company Phone", this.companyId.company_phone],
+            ["Company Website", this.companyId.company_website],
+            ["Company Address", this.companyId.company_address],
+
+            ["Human Rescuse *"],
+            ["HR Name", this.companyId.hr_name],
+            ["HR Email", this.companyId.hr_email],
+            ["HR Phone", this.companyId.hr_phone]
+        ];
+    })
     }
   },
   mounted() {
-    this.getUser();
+     if(localStorage.getItem('id') != undefined){
+        axios.get('/companies/'+ localStorage.getItem('id')).then(res=>{
+          this.user = res.data;
+          localStorage.setItem('companies',JSON.stringify(res.data));
+          this.getUserInfo();
+        })
+      }
   }
 };
 </script>
