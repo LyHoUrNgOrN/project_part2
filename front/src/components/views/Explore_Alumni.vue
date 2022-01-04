b0817ee855898a94fbb553f6538233afee1a9a3b
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2022-01-04 14:29:35 +0700

    #23 detail

--- front/src/components/views/Explore_Alumni.vue
+++ front/src/components/views/Explore_Alumni.vue
@@ -3,7 +3,6 @@
     :headers="headers"
     :items="desserts"
     :search="search"
-    @click:row="detail"
     sort-by="calories"
     class="elevation-1"
   >
@@ -60,6 +59,7 @@
       ></v-img>
     </template>
     <template v-slot:item.actions="{ item }">
+      <v-icon color="gray" left @click="detail(item)">mdi mdi-alert-box</v-icon>
       <v-icon color="red" small @click="deleteItem(item)"> mdi-delete </v-icon>
     </template></v-data-table
   >
@@ -125,18 +125,32 @@ export default {
 
         .then((result) => {
           result.data.forEach((element) => {
-            if (element.role == "ALUMNI") {
+            if (element.role === "ALUMNI") {
+              console.log(element.role);
               if (element.company == null) {
-                let explore = {
-                  id: element.id,
-                  profile: element.user_details.picture,
-                  first_name: element.first_name,
-                  last_name: element.last_name,
-                  pnc_batch: element.user_details.batch,
-                  current_position: "Alumni not yet complete!",
-                  company: "Alumni not yet complete!",
-                };
-                this.desserts.push(explore);
+                if(element.user_details == null){
+                  let explore = {
+                    id: element.id,
+                    profile: null,
+                    first_name: element.first_name,
+                    last_name: element.last_name,
+                    pnc_batch: "Alumni not yet complete",
+                    current_position: "Alumni not yet complete!",
+                    company: "Alumni not yet complete!",
+                  };
+                  this.desserts.push(explore);
+                }else{
+                  let explore = {
+                    id: element.id,
+                    profile: element.user_details.picture,
+                    first_name: element.first_name,
+                    last_name: element.last_name,
+                    pnc_batch: element.user_details.batch,
+                    current_position: "Alumni not yet complete!",
+                    company: "Alumni not yet complete!",
+                  };
+                  this.desserts.push(explore);
+                }
               } else {
                 let explore = {
                   id: element.id,
